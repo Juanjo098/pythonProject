00d7441c0e92f8292304fce69a7a696ec0e6a697
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2022-10-03 19:08:20 -0500

    Show result update

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -11,10 +11,10 @@ Creado por:
 """
 import numpy as np
 
-array = np.array([[3, 4, -10], [9, 2, -1], [7, 8, 5]], dtype="float32")
-results = np.array([-2, 3, 6], dtype="float32")
-#array = 0
-#results = 0
+#array = np.array([[3, 4, -10], [9, 2, -1], [7, 8, 5]], dtype="float32")
+#results = np.array([-2, 3, 6], dtype="float32")
+array = 0
+results = 0
 iteration_limit = 0
 tolerance_percentage = 0.0
 
@@ -49,6 +49,11 @@ def input_float(promt):
         return input_float(promt)
 
 def define_matrix():
+    """
+    Define la dimencion de la matriz de coeficientes, inserta los valores de los coeficientes y los resultados, establece
+    el número máximo de iteraciones a realizar y el porcentaje de tolerancia al error
+    :return: Void
+    """
     global array
     global results
     global iteration_limit
@@ -58,16 +63,19 @@ def define_matrix():
         size = input_int("Ingresa el tamaño de la matriz: ")
         if size > 1: break
         else: print("El tamaño de la matriz no puede ser menor a 0\n")
+    print("")
 
     while True:
         iteration_limit = input_int("Ingresa el número máximo de iteraciones: ")
         if iteration_limit > 0: break
         else: print("El límite de iteraciones debe ser mayor a 0\n")
+    print("")
 
     while True:
         tolerance_percentage = input_float("Ingresa el margen de tolerancia: ")
         if tolerance_percentage > 0: break
         else: print("El margen de tolerancia debe ser mayor a 0\n")
+    print("")
 
     tmp_array = []
     tmp_res = []
@@ -78,9 +86,11 @@ def define_matrix():
             tmp.append(input_float(f"Ecuación {row+1}, x{column + 1}: "))
         tmp_array.append(tmp)
         tmp_res.append(input_float(f"Resultado de la ecuación {row + 1}: "))
+        print("")
 
     array = np.array(tmp_array, dtype="float64")
     results = np.array(tmp_res, dtype="float64")
+
 
 def sort_array():
     global array
@@ -94,17 +104,27 @@ def sort_array():
             break
         else:
          if v[i] == v[i+1]:
-            print("Ey eso no se vale")
+            print("El sistema de ecuaciones no se puede resolver por el método de Gauss-Seidel")
             return False
     ordenada = []
     resord = []
-    #print()
-    for o in range(len(v)):
-        ordenada.insert(o,array[v.index(o)])
-        resord.insert(o,results[v.index(o)])
-        #print(ordenada[o]," --> ",resord[o])
-    array = np.array(ordenada, dtype="float32")
-    results = np.array(resord, dtype="float32")
+
+    ban = False
+    for i in range(1, len(v)):
+        if (v[i] != v[i - 1] + 1):
+            ban = True
+            break
+
+    if ban:
+        print("La matriz requiere acomodo...")
+        for o in range(len(v)):
+            ordenada.insert(o, array[v.index(o)])
+            resord.insert(o, results[v.index(o)])
+        array = np.array(ordenada, dtype="float32")
+        results = np.array(resord, dtype="float32")
+        print("Matriz reacomodada: ")
+        show_array(array,results)
+
     return True
 
 def process(array, results, iteration_limit: int, tolerance_percentage: float):
@@ -141,15 +161,15 @@ def process(array, results, iteration_limit: int, tolerance_percentage: float):
 
         #Determina si el mayor error aproximado de las icognitas esta por debajo del procentaje de tolerancia de error
         if np.amax(np.absolute(errors)) < tolerance_percentage:
-            print(f"Se alcanzó el porcentaje de tolerancia en la iteración: {iteration}")
-            show_array(array,results)
+            print(f"Se alcanzó el porcentaje de tolerancia en la iteración: {iteration}\n")
+            print("Valores de las incógnitas: ")
             show_results(xs)
             return xs
 
         # Determina si se alcanzó el límite de iteraciones
         if iteration == iteration_limit:
-            print("Se alcanzó el límite de iteraciones")
-            show_array(array, results)
+            print("Se alcanzó el límite de iteraciones.\n")
+            print("Valores de las incógnitas: ")
             show_results(xs)
             return xs
 
@@ -167,12 +187,15 @@ def show_results(xs):
     for r in range(len(xs)):
         print(f"x{r+1}: {xs[r]}")
 
-def show_array(array, resulst):
+def show_array(array, results):
     for row in range(len(array)):
         for column in range(len(array[row])):
-                print((f"+{array[row][column]}" if array[row][column] > 0 else f"+{array[row][column]}"), end="")
-        print(f"={results[row]}")
+                print((f"{array[row][column]} " if array[row][column] > 0 else f"{array[row][column]} ") if column == 0 else (f"+ {array[row][column]} " if array[row][column] > 0 else f"{array[row][column]} "), end="")
+        print(f"= {results[row]}")
+    print("")
 
 define_matrix()
+print("Matriz de entrada: ")
+show_array(array, results)
 if sort_array():
     process(array, results, iteration_limit, tolerance_percentage)
